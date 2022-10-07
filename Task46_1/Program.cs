8ab3f82274b6ef50d5dd17c1a118456ebbffc7e7
Maria Yutaeva <mariya1982@yandex.ru>
2022-10-07 11:46:37 +0300

    задача46Сапрыкин

---
+++ Task46_1/Program.cs
@@ -0,0 +1,41 @@
+﻿// САПРЫКИН !!! Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+Random random = new Random();
+int rows = random.Next(4, 8);
+int columns = random.Next(4, 8);
+Console.WriteLine($"Размер данного массива {rows} х {columns}: ");
+int[,] numbers = new int[rows, columns];
+FillArray(numbers);
+PrintArray(numbers);
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1); 
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);   
+    Random random = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(-100, 1000);
+        }
+    }
+}
