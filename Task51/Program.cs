089f6f7c505c800810db87a6f976e3ecce601a39
Maria Yutaeva <mariya1982@yandex.ru>
2022-10-07 13:35:33 +0300

    задача51Сапрыкин

---
+++ Task51/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 51. Задайте двумерный массив. найдите сумму элементов, находящихся на главной диагонали, 
+// с индексами 0,0  1,1  2,2и т д.
+
+
+Random random = new Random();
+int rows = random.Next(5,6);
+int columns = random.Next(5,6);
+int[,] numbers = new int[rows, columns];
+Console.WriteLine($"Размер данного массива {rows} х {columns}: ");
+
+FillArray(numbers);
+
+SummNumbers(numbers);
+PrintArray(numbers);
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1); 
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);   
+    Random random = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(0, 10);
+        }
+    }
+}
+void SummNumbers(int[,] numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (i == j) 
+            {
+                sum = sum + numbers[i,j];
+            }
+        }
+    }
+    Console.WriteLine("Сумма элементов массива по главной диагонали = " + sum);
+}
